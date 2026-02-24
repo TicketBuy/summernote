d66ec5fd68f934d4e9b71ac0a4f47164e33b7626
RenoLooijmans <73367951+RenoLooijmans@users.noreply.github.com>
2026-02-25 00:24:36 +0100

    Update build

--- dist/summernote-bs4.js
+++ dist/summernote-bs4.js
@@ -5,7 +5,7 @@ https://summernote.org
 Copyright 2013- Hackerwins and contributors
 Summernote may be freely distributed under the MIT license.
 
-Date: 2026-02-24T22:57Z
+Date: 2026-02-24T23:24Z
  */
 import $ from "jquery";
 $.summernote = $.summernote || {
